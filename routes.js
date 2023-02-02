80a20e80b59949a89101501f08ceeca7dd57328f
Yashwanth Muddana <77020506+yashh2110@users.noreply.github.com>
2023-02-02 13:11:39 +0530

    initial commit

--- routes.js
+++ routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const userRoute = require("./modules/user-auth/user.controller");
 
 const routes = express.Router();
-
+routes.get("/", (req, res) => res.json("hey there hello "));
 routes.use("/user", userRoute);
 
 module.exports = routes;
